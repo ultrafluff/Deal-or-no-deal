01aa1b84fbd14a19f6626ea3522279cc9c722032
ultrafluff <ultrafluffzander@gmail.com>
2024-06-19 18:05:51 -0500

    added the randomselector function for the amounts

--- javascript.js
+++ javascript.js
@@ -4,3 +4,9 @@ const amounts =
 1000, 2500, 5000, 7500, 10000,
 15000, 25000, 50000, 100000, 250000
 ]
+
+function randomselector() {
+    let amount = (amounts[Math.floor(Math.random()*amounts.length)]);
+    let index = amounts.indexOf(amount)
+    amounts.splice(amounts.indexOf(amount), 1);
+}
