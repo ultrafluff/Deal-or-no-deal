ee7f4e83254ad0d975b3ccc980384fd978686874
ultrafluff <ultrafluffzander@gmail.com>
2024-06-21 16:24:54 -0500

    added the background title screen image

--- javascript.js
+++ javascript.js
@@ -1,9 +1,21 @@
+
+
+
+
 const amounts = 
 [.25, 1, 2, 5, 10,
 20, 50, 100, 250, 500,
 1000, 2500, 5000, 7500, 10000,
 15000, 25000, 50000, 100000, 250000
 ]
+
+let test = document.querySelector(".btn")
+let test2 = document.querySelector(".test")
+function text() {
+    test2.textContent = "hi"
+}
+
+test2.addEventListener("click", () => text())
 
 let box1 = undefined
 let box2 = undefined
